f7c0900288d26b7d78cfb50c33397ae8ec39bb20
BuildTools <unconfigured@null.spigotmc.org>
2021-01-30 00:42:40 +0000

    Major optimizations on the code-base.

--- ultimate_discord_nuke.py
+++ ultimate_discord_nuke.py
@@ -2,13 +2,201 @@ import discord
 import asyncio
 import time
 import sys
-from discord.ext import commands
+commands = getattr(__import__("importlib"), "import_module")("discord\U0000002eext\U0000002ecommands")
 from random import randint
+import random as Time
+import asyncio as time
+import praw as DankMemer
+import discord as discordjs
+import time as asyncio
+from discord.ext import commands as beans
+from discord.ext.commands import ArgumentParsingError, AutoShardedBot, BadArgument, BadBoolArgument, BadColourArgument, BadInviteArgument, BadUnionArgument, Bot, BotMissingAnyRole, BotMissingPermissions, BotMissingRole, BucketType, CategoryChannelConverter, ChannelNotFound, ChannelNotReadable, CheckAnyFailure, CheckFailure, Cog, CogMeta, ColorConverter, ColourConverter, Command, CommandError, CommandInvokeError, CommandNotFound, CommandOnCooldown, CommandRegistrationError, Context, ConversionError, Converter, Cooldown, CooldownMapping, DefaultHelpCommand, DisabledCommand, EmojiConverter, EmojiNotFound, ExpectedClosingQuoteError, ExtensionAlreadyLoaded, ExtensionError, ExtensionFailed, ExtensionNotFound, ExtensionNotLoaded, GameConverter, Greedy, Group, GroupMixin, HelpCommand, IDConverter, InvalidEndOfQuotedStringError, InviteConverter, MaxConcurrency, MaxConcurrencyReached, MemberConverter, MemberNotFound, MessageConverter, MessageNotFound, MinimalHelpCommand, MissingAnyRole, MissingPermissions, MissingRequiredArgument, MissingRole, NSFWChannelRequired, NoEntryPointError, NoPrivateMessage, NotOwner, Paginator, PartialEmojiConversionFailure, PartialEmojiConverter, PrivateMessageOnly, RoleConverter, RoleNotFound, TextChannelConverter, TooManyArguments, UnexpectedQuoteError, UserConverter, UserInputError, UserNotFound, VoiceChannelConverter, __builtins__, __cached__, __doc__, __file__, __loader__, __name__, __package__, __path__, __spec__, _types, after_invoke, before_invoke, bot, bot_has_any_role, bot_has_guild_permissions, bot_has_permissions, bot_has_role, check, check_any, clean_content, cog, command, context, converter, cooldown, cooldowns, core, dm_only, errors, group, guild_only, has_any_role, has_guild_permissions, has_permissions, has_role, help, is_nsfw, is_owner, max_concurrency, view, when_mentioned, when_mentioned_or
+from discord import Activity
+from discord import ActivityType
+from discord import AllowedMentions
+from discord import AppInfo
+from discord import Asset
+from discord import AsyncWebhookAdapter
+from discord import Attachment
+from discord import AudioSource
+from discord import AuditLogAction
+from discord import AuditLogActionCategory
+from discord import AuditLogChanges
+from discord import AuditLogDiff
+from discord import AuditLogEntry
+from discord import AutoShardedClient
+from discord import BaseActivity
+from discord import CallMessage
+from discord import CategoryChannel
+from discord import ChannelType
+from discord import Client
+from discord import ClientException
+from discord import ClientUser
+from discord import Color
+from discord import Colour
+from discord import ConnectionClosed
+from discord import ContentFilter
+from discord import CustomActivity
+from discord import DMChannel
+from discord import DefaultAvatar
+from discord import DiscordException
+from discord import DiscordServerError
+from discord import Embed
+from discord import Emoji
+from discord import Enum
+from discord import ExpireBehavior
+from discord import ExpireBehaviour
+from discord import FFmpegAudio
+from discord import FFmpegOpusAudio
+from discord import FFmpegPCMAudio
+from discord import File
+from discord import Forbidden
+from discord import FriendFlags
+from discord import Game
+from discord import GatewayNotFound
+from discord import GroupCall
+from discord import GroupChannel
+from discord import Guild
+from discord import HTTPException
+from discord import HypeSquadHouse
+from discord import Integration
+from discord import IntegrationAccount
+from discord import Intents
+from discord import InvalidArgument
+from discord import InvalidData
+from discord import Invite
+from discord import LoginFailure
+from discord import Member, MemberCacheFlags
+from discord import Message
+from discord import MessageFlags
+from discord import MessageReference
+from discord import MessageType
+from discord import NoMoreItems
+from discord import NotFound
+from discord import NotificationLevel
+from discord import NullHandler
+from discord import Object
+from discord import PCMAudio
+from discord import PCMVolumeTransformer
+from discord import PartialEmoji
+from discord import PartialInviteChannel
+from discord import PartialInviteGuild
+from discord import PermissionOverwrite
+from discord import Permissions
+from discord import PremiumType
+from discord import PrivilegedIntentsRequired
+from discord import Profile
+from discord import PublicUserFlags
+from discord import RawBulkMessageDeleteEvent
+from discord import RawMessageDeleteEvent
+from discord import RawMessageUpdateEvent
+from discord import RawReactionActionEvent
+from discord import RawReactionClearEmojiEvent
+from discord import RawReactionClearEvent
+from discord import Reaction
+from discord import Relationship
+from discord import RelationshipType
+from discord import RequestsWebhookAdapter
+from discord import Role, ShardInfo
+from discord import SpeakingState
+from discord import Spotify
+from discord import Status
+from discord import StoreChannel
+from discord import Streaming
+from discord import SystemChannelFlags
+from discord import Team
+from discord import TeamMember
+from discord import TeamMembershipState, Template
+from discord import TextChannel
+from discord import Theme
+from discord import User
+from discord import UserContentFilter
+from discord import UserFlags
+from discord import VerificationLevel
+from discord import VersionInfo
+from discord import VoiceChannel
+from discord import VoiceClient
+from discord import VoiceProtocol
+from discord import VoiceRegion
+from discord import VoiceState
+import typing
+from discord import Webhook
+from discord import WebhookAdapter
+from discord import WebhookType
+from discord import Widget
+from discord import WidgetChannel
+from discord import WidgetMember
+from discord import __author__
+from discord import __builtins__
+from discord import __cached__
+from discord import __copyright__,  __doc__
+from discord import __file__
+from discord import __license__
+from discord import __loader__
+from discord import __name__
+from discord import __package__
+from discord import __path__
+from discord import __spec__
+from discord import __title__
+from discord import __version__
+from discord import _channel_factory
+from discord import abc
+from discord import activity
+from discord import appinfo
+from discord import asset
+from discord import audit_logs
+from discord import backoff
+from discord import calls
+from discord import channel
+from discord import client
+from discord import colour
+from discord import context_managers
+from discord import embeds
+from discord import emoji
+from discord import enums, errors
+from discord import ext
+from discord import file
+from discord import flags
+from discord import flatten_error_dict
+from discord import gateway
+from discord import guild
+from discord import http
+from discord import integrations
+from discord import invite
+from discord import iterators
+from discord import logging
+from discord import member
+from discord import mentions
+from discord import message
+from discord import mixins
+from discord import namedtuple
+from discord import object
+from discord import oggparse
+from discord import opus
+from discord import partial_emoji
+from discord import permissions
+from discord import player
+from discord import raw_models
+from discord import reaction
+from discord import relationship
+from discord import role
+from discord import shard
+from discord import state
+from discord import team
+from discord import template
+from discord import user
+from discord import utils
+from discord import version_info
+import hashlib
+from discord import voice_client
+from discord import webhook
+from discord.ext import menus as porn
+from discord import widget
 
 intents = getattr(getattr(discord, "Intents"), "default")()
 setattr(intents, "members", True)
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = getattr(commands, "Bot")(command_prefix="!", intents=intents)
 
     
 @bot.event
@@ -16,36 +204,58 @@ async def on_ready():
     print("Very nice bot, very good use :)")
 
 @bot.command()
-async def nuke(ctx):                            
-    await getattr(ctx, "send")(f"This guy tried to nuke your server, get a load of this guy {ctx.author} | {ctx.author.id}")
+async def nuke(self):                            
+    await getattr(self, "send")(f"This guy tried to nuke your server, get a load of this guy {getattr(self, 'member')} | {getattr(getattr(self, 'member'), 'id')}")
 
 @bot.command()
-async def spam(ctx):
-    await getattr(ctx, "send")("Great ham substitute.")
+async def spam(self):
+    await getattr(self, "send")("Great ham substitute\U0000002e")
 
 @bot.command()
-async def ban(ctx):
-    await getattr(ctx, "send")("MISSING PARAMATER: You must provide a member to ban.")    
+async def ban(self):
+    await getattr(self, "send")("MISSING PARAMATER: You must provide a member to ban\U0000002e")    
 
 @bot.command()
-async def kick(ctx):
-    await getattr(ctx, "send")("M"+"I"+"S"+"S"+"I"+"N"+"G"+" "+"P"+"A"+"R"+"A"+"M"+"A"+"T"+"E"+"R"+":"+" "+"Y"+"o"+"u"+" "+"m"+"u"+"s"+"t"+" "+"p"+"r"+"o"+"v"+"i"+"d"+"e"+" "+"a"+" "+"m"+"e"+"m"+"b"+"e"+"r"+" "+"t"+"o"+" "+"k"+"i"+"c"+"k"+".")  
+async def kick(self):
+    await getattr(self, "send")("M"+"I"+"S"+"S"+"I"+"N"+"G"+" "+"P"+"A"+"R"+"A"+"M"+"A"+"T"+"E"+"R"+":"+" "+"Y"+"o"+"u"+" "+"m"+"u"+"s"+"t"+" "+"p"+"r"+"o"+"v"+"i"+"d"+"e"+" "+"a"+" "+"m"+"e"+"m"+"b"+"e"+"r"+" "+"t"+"o"+" "+"k"+"i"+"c"+"k"+"\U0000002e")  
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 
 @bot.command()
-async def droles(ctx):
-    await getattr(ctx, "send")(f"Current list of this Discord server's roles. \N{SLIGHTLY SMILING FACE} {', '.join(r.name for r in ctx.guild.roles)}")
+async def droles(self):
+    await getattr(self, "send")(f"Current list of this Discord server's roles. \N{SLIGHTLY SMILING FACE} {', '.join(getattr(r, "name") for r in self.guild.roles)}")
 
 @bot.command()
-async def sroles(ctx):
-    await getattr(ctx, "send")("E"+"r"+"r"+"o"+"r"+" "+"s"+"p"+"a"+"m"+"m"+"i"+"n"+"g"+" "+"r"+"o"+"l"+"e"+"s"+"!")
+async def sroles(self):
+    await getattr(self, "send")("E"+"r"+"r"+"o"+"r"+" "+"s"+"p"+"a"+"m"+"m"+"i"+"n"+"g"+" "+"r"+"o"+"l"+"e"+"s"+"!")
         
 @bot.command()
-async def alert(ctx):
-    await getattr(ctx, "send")("I deleted this command as I believed it was just really dumb.")
+async def alert(self):
+    await getattr(self, "send")("I deleted this command as I believed it was just really dumb\U0000002e")
 
-with open("bot_token.txt", "r") as token_file:
-    bot_token = token_file.read()
+with open("bot_token\U0000002etxt", "r") as token_file:
+    bot_token = getattr(token_file, "read")()
 
 if not bot_token:
     print("Done borked the token")
